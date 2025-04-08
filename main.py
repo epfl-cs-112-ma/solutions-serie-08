70894f0cac9ef7badcdc58ee79fc87bb83a4d232
Sébastien Doeraene <sjrdoeraene@gmail.com>
2025-04-08 10:57:01 +0200

    Finish the exercise.

--- main.py
+++ main.py
@@ -8,13 +8,24 @@ def main() -> None:
         library_name = "arcade" # some default, because why not?
 
     url = f"https://pypi.org/pypi/{library_name}/json"
-    response = requests.get(url)
 
-    if response.status_code != 200:
-        print("The HTTP request did not complete successfully:")
-        print(f"{response.status_code} {response.reason}")
-    else:
-        print(response.text)
+    try:
+        response = requests.get(url)
+
+        if response.status_code != 200:
+            print("The HTTP request did not complete successfully:")
+            print(f"{response.status_code} {response.reason}")
+        else:
+            json: object = response.json()
+            match json:
+                case {'info': {'version': str() as version_string}}:
+                    print(version_string)
+                case _:
+                    print("Unexpected response:")
+                    print(repr(json))
+    except requests.exceptions.RequestException as e:
+        print("Network error:")
+        print(e)
 
 
 if __name__ == "__main__":
