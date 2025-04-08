d80f8d2567a258f09412cc6b97705febcd023dc8
Sébastien Doeraene <sjrdoeraene@gmail.com>
2025-04-08 10:33:41 +0200

    Make a request and output the raw text response.

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import requests
+import sys
+
 def main() -> None:
-    print("Hello from solutions-serie-08!")
+    if len(sys.argv) > 1:
+        library_name = sys.argv[1]
+    else:
+        library_name = "arcade" # some default, because why not?
+
+    url = f"https://pypi.org/pypi/{library_name}/json"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print("The HTTP request did not complete successfully:")
+        print(f"{response.status_code} {response.reason}")
+    else:
+        print(response.text)
 
 
 if __name__ == "__main__":
